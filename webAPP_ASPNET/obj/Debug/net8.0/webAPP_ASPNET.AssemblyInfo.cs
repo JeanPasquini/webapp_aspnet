b422c55bce17ebfecc827f4dee18ec9d93d2b9fb
Pasquini <pasquinijean@hotmail.com>
2024-12-12 08:45:57 -0300

    2024-12-12 Finalizando projeto Connection ASPNET

--- webAPP_ASPNET/obj/Debug/net8.0/webAPP_ASPNET.AssemblyInfo.cs
+++ webAPP_ASPNET/obj/Debug/net8.0/webAPP_ASPNET.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webAPP_ASPNET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e04dba59a0a4e67afa2054db20733fbb4d36779b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2442efbe18bb5a8c57af84797faa37a849b9b914")]
 [assembly: System.Reflection.AssemblyProductAttribute("webAPP_ASPNET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webAPP_ASPNET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
